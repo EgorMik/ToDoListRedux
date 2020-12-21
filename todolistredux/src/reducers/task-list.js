0081c3068ecbaa7f6bd389e694aab1706ee70e9c
Egor <egormikulic@gmail.com>
2020-12-21 11:25:25 +0300

    Change1

---
+++ todolistredux/src/reducers/task-list.js
@@ -0,0 +1,22 @@
+const updateList = (state, action) => {
+
+    if (state === undefined) {
+      return {
+        items: [],
+        loading: true,
+        error: null
+      };
+    }
+    switch (action.type) {
+        case "ADD_TODO":
+          return [
+            ...state,
+            {
+              id: action.id,
+              complete: action.complete,
+              text: action.text
+            }
+          ];
+        }
+};
+export default updateList;
